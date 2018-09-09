5957bcb9204a5cd0049dec9e2102725d9ae70ded
t1mmmmY <t1mmmmY111@gmail.com>
2018-09-09 22:18:03 +0200

    Space ship with the hole

---
+++ Assets/Game/Scripts/Common/Door.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour 
+{
+	[SerializeField] Animator doorAnimator;
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.tag == "Human")
+		{
+			doorAnimator.SetBool("character_nearby", true);
+		}
+	}
+
+	void OnTriggerExit(Collider other)
+	{
+		if (other.tag == "Human")
+		{
+			doorAnimator.SetBool("character_nearby", false);
+		}
+	}
+}
